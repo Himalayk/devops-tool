b730a6f14cb1ca761582108cecb68a6e8afd174c
Himalayk <kondekarhimalay@gmail.com>
2017-04-19 14:48:25 +0530

    dbconfig change

--- dbconfig.php
+++ dbconfig.php
@@ -1,9 +1,9 @@
 <?php
 
-	define('DB_HOST', 'localhost');
-	define('DB_USER', 'root');
-	define('DB_PASSWORD', '');
-	define('DB_DATABASE', 'DevAT');
+	define('DB_HOST', 'localhost'); 
+	define('DB_USER', 'root'); //Keep as it is when your mysql is on localhost
+	define('DB_PASSWORD', ''); //Give a password what u use to login mysql
+	define('DB_DATABASE', 'DevAT'); //Create database in mysql and use name here
 	
 	$link = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD);
 	if(!$link) {
